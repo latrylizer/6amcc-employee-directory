926c7d522952d2a5c92818d226b9687f7c9add78
Latryll <latryll.alimes@citylightcs>
2021-12-22 03:06:33 +0800

    2nd commit

--- src/redux/reducers/allEmployees.js
+++ src/redux/reducers/allEmployees.js
@@ -1,35 +1,5 @@
 import { SET_ALL_EMPLOYEES } from '../types'
 
-const initialState = [
-    {
-        id: 1,
-        name: 'Latryll Alimes',
-        position: 'Remote Team Member',
-        email: 'latrylizer@gmail.com',
-        hireDate: 'November 16',
-        contact: 555325481,
-        department: '6AMCC',
-    },
-    {
-        id: 2,
-        name: 'Kyn Jupilou Empremiado',
-        position: 'Remote Team Member',
-        email: 'latrylizer@gmail.com',
-        hireDate: 'November 16',
-        contact: 555325481,
-        department: '6AMCC',
-    },
-    {
-        id: 3,
-        name: 'Sheanne Noreen Bernardino',
-        position: 'Remote Team Member',
-        email: 'latrylizer@gmail.com',
-        hireDate: 'November 16',
-        contact: 555325481,
-        department: '6AMCC',
-    },
-]
-
 const AllEmployeeReducers = (state = [], action = {}) => {
     switch (action.type) {
         case SET_ALL_EMPLOYEES:
